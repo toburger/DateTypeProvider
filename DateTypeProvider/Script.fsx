be0e249709aa265bfe3a5aa858e889d0310bcbea
Tobias Burger <tobias_burger@hotmail.com>
2014-07-25 12:03:07 +0200

    update script with more examples

--- DateTypeProvider/Script.fsx
+++ DateTypeProvider/Script.fsx
@@ -17,3 +17,13 @@ printfn "%A" <| C21.``2012``.November.``01``.ToDateTime()
 
 type C18 = Date<century = Century.``18``>
 printfn "%A" <| C18.``1789``.July.``14``.ToDateTime()
+
+// you can force a century even if it is not defined in the enum
+let [<Literal>] c80: Century = enum 80
+type C80 = Date<century = c80>
+printfn "%A" <| C80.``7908``.July.``14``.ToDateTime()
+
+// providing an invalid Century falls back to the current century
+let [<Literal>] c100: Century = enum 100
+type C100 = Date<century = c100>
+printfn "%A" <| C100.``2004``.July.``14``.ToDateTime()
